60cc4218a6210f669d5552d65289c9ebb9b850e1
vborodii <vborodii@cybervisiontech.com>
2018-03-09 14:17:13 +0200

    Added insertNestedDocument function.

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const maprClient = require('mapr-node-bindings')
 const config = require('./config')
 
 const {MAPRDB_HOST, MAPRDB_PORT} = config
-const { Document, StoreConnection, ODate, OInterval, OTime, OTimestamp } = maprClient
+const {Document, StoreConnection, ODate, OInterval, OTime, OTimestamp} = maprClient
 
 const maprURL = `${MAPRDB_HOST}:${MAPRDB_PORT}`
 const tableName = '/test-db-1'
@@ -15,29 +15,59 @@ function createTable() {
         console.log('tableExists', {err, result})
         if (result) {
             insertDocument()
+            insertNestedDocument()
         } else {
             store.createTable(tableName, (err, result) => {
                 console.log('createTable', {err, result})
                 insertDocument()
+                insertNestedDocument()
             })
         }
     })
 }
 
 function insertDocument() {
-    const docId = 123;
+    const docId = '123';
     const table = store.getStore(tableName)
     const doc = new Document(docId)
-      .setField('test_int', 123)
-      .setField('test_date', new ODate(1518689532))
-      .setField('test_interval', new OInterval(172800000))
-      .setField('test_time', new OTime(3456))
-      .setField('test_timestamp', new OTimestamp(29877132000))
-      .setField('test_bool', true)
-      .setField('test_bool_false', false)
-      .setField('test_str', 'strstr')
-      .setField('test_object', {a: 1, b: 2})
-      .setField('test_empty_object', {})
+        .setField('test_int', 123)
+        .setField('test_date', new ODate(1518689532))
+        .setField('test_time', new OTime(3456))
+        .setField('test_timestamp', new OTimestamp(29877132))
+        .setField('test_bool', true)
+        .setField('test_bool_false', false)
+        .setField('test_str', 'strstr')
+        .setField('test_object', {a: 1, b: 2})
+        .setField('test_empty_object', {})
+
+    table.insertOrReplace(doc.toJSON(), tableName, (err, result) => {
+        console.log('insertOrReplace', {err, result})
+        findDocument(docId)
+    })
+}
+
+function insertNestedDocument() {
+    const docId = '1234';
+    const table = store.getStore(tableName)
+    const doc = new Document(docId)
+        .setField('first.test_int', 123)
+        .setField('first.test_date', new ODate(1518689532))
+        .setField('first.test_time', new OTime(3456))
+        .setField('first.test_timestamp', new OTimestamp(29877132))
+        .setField('first.test_bool', true)
+        .setField('first.test_bool_false', false)
+        .setField('first.test_str', 'strstr')
+        .setField('first.test_object', {a: 1, b: 2})
+        .setField('first.test_empty_object', {})
+        .setField('first.second.test_int', 123)
+        .setField('first.second.test_date', new ODate(1518689532))
+        .setField('first.second.test_time', new OTime(3456))
+        .setField('first.second.test_timestamp', new OTimestamp(29877132))
+        .setField('first.second.test_bool', true)
+        .setField('first.second.test_bool_false', false)
+        .setField('first.second.test_str', 'strstr')
+        .setField('first.second.test_object', {a: 1, b: 2})
+        .setField('first.second.test_empty_object', {})
 
     table.insertOrReplace(doc.toJSON(), tableName, (err, result) => {
         console.log('insertOrReplace', {err, result})
