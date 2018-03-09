768e7ddfb75912c1f55b14d59035a7fea974f7c6
vborodii <vborodii@cybervisiontech.com>
2018-03-09 13:18:25 +0200

    add insertDocument fields

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const maprClient = require('mapr-node-bindings')
 const config = require('./config')
 
 const {MAPRDB_HOST, MAPRDB_PORT} = config
-const { Document, StoreConnection } = maprClient
+const { Document, StoreConnection, ODate, OInterval, OTime, OTimestamp } = maprClient
 
 const maprURL = `${MAPRDB_HOST}:${MAPRDB_PORT}`
 const tableName = '/test-db-1'
@@ -26,11 +26,20 @@ function createTable() {
 
 function insertDocument() {
     const table = store.getStore(tableName)
-    const doc = new Document('test_id')
-    doc.setField('field', 'value')
+    const doc = new Document(123)
+      .setField('test_int', 123)
+      .setField('test_date', new ODate(1518689532))
+      .setField('test_interval', new OInterval(172800000))
+      .setField('test_time', new OTime(3456))
+      .setField('test_timestamp', new OTimestamp(29877132000))
+      .setField('test_bool', true)
+      .setField('test_bool_false', false)
+      .setField('test_str', 'strstr')
+      .setField('test_object', {a: 1, b: 2})
+      .setField('test_empty_object', {})
 
     table.insertOrReplace(doc.toJSON(), tableName, (err, result) => {
-        console.log('insertOrReplace', doc.toJSON(), {err, result})
+        console.log('insertOrReplace', {err, result})
     })
 }
 
